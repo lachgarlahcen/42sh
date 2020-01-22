7d6dc07653fa34c9d85c51f38d0dee50e90d4780
aihya <aihya@student.1337.ma>
2020-01-22 20:00:18 +0100

    hashtable functions update

--- src/alias.c
+++ src/alias.c
@@ -6,7 +6,7 @@
 /*   By: aihya <aihya@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 18:37:49 by aihya             #+#    #+#             */
-/*   Updated: 2020/01/22 19:26:08 by aihya            ###   ########.fr       */
+/*   Updated: 2020/01/22 19:58:21 by aihya            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ t_htnode	**init_hashtable()
 	t_htnode	**hashtable;
 	int			i;
 
-	table = (t_htnode **)malloc(sizeof(t_htnode *) * LIMIT);
+	table = (t_htnode **)malloc(sizeof(t_htnode *) * HT_LIMIT);
 	if (table == NULL)
 		return (NULL);
 	i = 0;
@@ -42,6 +42,42 @@ t_htnode	**init_hashtable()
 		i++;
 	}
 	return (table);
+}
+
+t_htnode	*new_htnode(char *name, char *value)
+{
+	t_htnode	*htnode;
+
+	htnode = (t_htnode *)malloc(sizeof(t_htnode));
+	if (htnode == NULL)
+		return (NULL);
+	htnode->name = ft_strdup(name);
+	if (name != NULL && htnode->name == NULL)
+		return (NULL);
+	htnode->value = ft_strdup(value);
+	if (name != NULL && htnode->value == NULL)
+		return (NULL);
+	htnode->next = NULL;
+	return (htnode);
+}
+
+void		push_htnode(t_htnode **hashtable, t_htnode *htnode)
+{
+	t_htnode	*node;
+	int			index;
+
+	index = hash_function(htnode->name);
+	node = hashtable[index];
+	while (node)
+	{
+		if (node->next == NULL)
+			break ;
+		node = node->next;
+	}
+	if (node == NULL)
+		hashtable[index] = htnode;
+	else
+		node->next = htnode;
 }
 
 
