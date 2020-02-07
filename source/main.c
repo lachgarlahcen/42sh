41ca61d9ea203a22ff782b0fa6d50d25f767dd70
Hamza Astid <hastid@e2r8p12.1337.ma>
2020-02-07 06:23:08 +0100

    check stdin && stdout

--- source/main.c
+++ source/main.c
@@ -6,17 +6,26 @@
 /*   By: llachgar <llachgar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/01 20:14:50 by hastid            #+#    #+#             */
-/*   Updated: 2020/02/07 05:42:36 by hastid           ###   ########.fr       */
+/*   Updated: 2020/02/07 06:01:33 by hastid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+
+int	init_shell(void)
+{
+	if (isatty(0) && isatty(1))
+		return (1);
+	return (0);
+}
 
 int main(int ac, char **av, char **env)
 {
 	int stat;
 	char *line;
 
+	if (!init_shell())
+		return (0);
 	(void)ac;
 	(void)av;
 	stat = 0;
