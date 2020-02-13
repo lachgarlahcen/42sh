d7f79a078cbd54b5a6d09490167c15e5abf51a95
Hamza Astid <hastid@e2r8p14.1337.ma>
2020-02-13 06:06:45 +0100

    file protect

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: llachgar <llachgar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/01 20:14:50 by hastid            #+#    #+#             */
-/*   Updated: 2020/02/12 23:45:11 by hastid           ###   ########.fr       */
+/*   Updated: 2020/02/13 06:05:45 by hastid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,24 +37,45 @@ int	exit_status(int status, int check)
 	return (e_status);
 }
 
+int	init_fd(void)
+{
+	int	fd;
+
+	if ((fd = open("/dev/tty", O_RDWR) == -1))
+		return (1);
+	if (dup2(222, fd) == -1)
+		return (1);
+	close(fd);
+	if (dup2(0, 222) == -1)
+		return (1);
+	if (dup2(1, 222) == -1)
+		return (1);
+	if (dup2(2, 222) == -1)
+		return (1);
+}
+
 int main(int ac, char **av, char **env)
 {
 	int stat;
 	char *line;
 
 	if (init_shell())
-		return (0);
+		return (1);
 	(void)ac;
 	(void)av;
 	stat = 0;
+	if (init_fd())
+		return (1);
 	signals(1);
 	init_history();
 	g_first_job = 0;
 	init_variables(env);
-	while ((line = read_line("42sh $> ")))
+	while (!stat && (line = read_line("42sh $> ")))
 	{
 		line_editing(&line, 1);
 		ft_memdel((void **)&line);
+		if (init_fd())
+			stat = 1;
 	}
 	free_variables();
 	free_history();
