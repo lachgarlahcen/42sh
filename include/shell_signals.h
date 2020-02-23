adc30d8d84e1afa38c125a721d285f96237923a7
llachgar <lachgarlahcen@hotmail.com>
2020-02-23 20:01:39 +0100

    remove leaks from signal liberary

--- include/shell_signals.h
+++ include/shell_signals.h
@@ -6,12 +6,12 @@
 /*   By: hastid <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 05:27:56 by hastid            #+#    #+#             */
-/*   Updated: 2020/02/07 05:28:02 by hastid           ###   ########.fr       */
+/*   Updated: 2020/02/23 19:07:45 by llachgar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SIGNALS_H
-# define SIGNALS_H
+#ifndef SHELL_SIGNALS_H
+# define SHELL_SIGNALS_H
 
 # include "shell.h"
 # include <sys/types.h>
@@ -19,6 +19,6 @@
 # include <unistd.h>
 # include <signal.h>
 
-void    signals(int c);
+void	signals(int c);
 
 #endif
--- source/shell_signals/src/shell_signals.c
+++ source/shell_signals/src/shell_signals.c
@@ -6,22 +6,16 @@
 /*   By: hastid <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 05:32:23 by hastid            #+#    #+#             */
-/*   Updated: 2020/02/07 05:43:17 by hastid           ###   ########.fr       */
+/*   Updated: 2020/02/23 19:06:24 by llachgar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell_signals.h"
 
-/*static void child_seg(int seg)
+static void		kill_seg(int seg)
 {
-	(void)seg;
-	update_status();
-}*/
-
-static void	kill_seg(int seg)
-{
-	t_cmd	*l;
-	char	buf[1];
+	t_cmd		*l;
+	char		buf[1];
 
 	(void)seg;
 	l = NULL;
@@ -37,22 +31,20 @@ static void	kill_seg(int seg)
 		ft_putchar('\n');
 }
 
-void    signals(int c)
-{   
-    if (c)
-    {
-		//signal (SIGCHLD, child_seg);
-        signal(SIGINT, kill_seg);
-        signal (SIGQUIT, SIG_IGN);
-        signal (SIGTSTP, SIG_IGN);
-        signal (SIGTTIN, SIG_IGN);
-        signal (SIGTTOU, SIG_IGN);
-        return ;
-    }
-    signal (SIGINT, SIG_DFL);
-    signal (SIGQUIT, SIG_DFL);
-    signal (SIGTSTP, SIG_DFL);
-    signal (SIGTTIN, SIG_DFL);
-    signal (SIGTTOU, SIG_DFL);
-	 //signal (SIGCHLD, SIG_DFL);//
+void			signals(int c)
+{
+	if (c)
+	{
+		signal(SIGINT, kill_seg);
+		signal(SIGQUIT, SIG_IGN);
+		signal(SIGTSTP, SIG_IGN);
+		signal(SIGTTIN, SIG_IGN);
+		signal(SIGTTOU, SIG_IGN);
+		return ;
+	}
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	signal(SIGTSTP, SIG_DFL);
+	signal(SIGTTIN, SIG_DFL);
+	signal(SIGTTOU, SIG_DFL);
 }
