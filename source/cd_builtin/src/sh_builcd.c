78e5ecdb260965a6b32cd608734afa05a1b80ba7
neostein <nsaber@e2r7p12.1337.ma>
2020-02-25 04:38:57 +0100

    fix jump errors in cd and ./././././ case

--- source/cd_builtin/src/sh_builcd.c
+++ source/cd_builtin/src/sh_builcd.c
@@ -6,7 +6,7 @@
 /*   By: nsaber <nsaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/13 02:26:55 by hastid            #+#    #+#             */
-/*   Updated: 2020/02/23 21:49:35 by nsaber           ###   ########.fr       */
+/*   Updated: 2020/02/25 04:36:58 by nsaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,33 +35,34 @@ static char	*del_slash(char *str)
 	return (ft_strdup(buff));
 }
 
-static char	*del_point(char *str)
+static char	*del_point(char *str,char	*tmp, int i, int p)
 {
-	int		i;
-	int		p;
 	char	buf[READ_SIZE];
-	char	*tmp;
 
-	i = 0;
-	p = 0;
 	ft_bzero(buf, READ_SIZE);
 	while (str[i] && p < READ_SIZE - 1)
 	{
-		(str[0] == '.' && str[i + 2] == '/') ? i += 1 : 0;
-		(str[i] == '/' && str[i + 1] == '.' && !str[i + 2]) ? i += 2 : 0;
-		if (str[i] == '/' && str[i + 1] == '.' && str[i + 2] == '/')
-			i += 2;
-		if (str[i] == '.' && str[i + 1] == 0 && str[i - 1] != '.')
+		(str[0] == '.' && (str[i + 2] == '/')) ? i += 1 : 0;
+		if(str[i] == '.' && str[i + 1] == 0 && str[i - 1] != '.')
+		{
 			i += 1;
+			break;
+		} 
+		if (str[i] == '/' && str[i + 1] == '.' && !str[i + 2])
+		{
+			i += 2;
+			break;
+		} 
+		(str[i] == '/' && str[i + 1] == '.' && str[i + 2] == '/') ? i += 2 : 0;
 		buf[p++] = str[i++];
 	}
 	buf[i] = '\0';
 	if (ft_strstr(buf, "/./"))
-		tmp = del_point(ft_strdup(buf));
+		tmp = del_point(ft_strdup(buf), NULL, 0, 0);
 	else
-		tmp = buf;
+		tmp = ft_strdup(buf);
 	ft_memdel((void **)&str);
-	return (ft_strdup(tmp));
+	return (tmp);
 }
 
 static char	**tab_path(char *str)
@@ -137,7 +138,7 @@ char		*path_of_link(char *dir, char *pwd)
 			return (0);
 		ft_memdel((void **)&tp);
 	}
-	tmp2 = del_point(tmp);
+	tmp2 = del_point(tmp, NULL, 0, 0);
 	tmp = del_slash(tmp2);
 	tmp2 = del_point2(tmp);
 	return (tmp2);
